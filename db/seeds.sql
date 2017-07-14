128795cbe1502e88a5f77023f94a2672e3acd647
ighobverhai aror <ighobverhai aror>
2017-07-14 09:31:50 -0400

    sequelised

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (Burger_name)
+VALUES ('Mushroom Burgers');
+INSERT INTO burgers (Burger_name)
+VALUES ('Cheese Burgers');
+INSERT INTO burgers (Burger_name)
+VALUES ('Bufallo Burgers');
